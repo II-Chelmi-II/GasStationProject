a8ba48dac06325f6c3a925ddfbe8ee18ba7a62b2
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-05-13 12:32:43 +0300

    chore: Add EvaporationRate model

--- src/main/java/com/michel/station/Model/EvaporationRate.java
+++ src/main/java/com/michel/station/Model/EvaporationRate.java
@@ -1,2 +1,13 @@
-package com.michel.station.Model;public class EvaporationRate {
+package com.michel.station.Model;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EvaporationRate {
+    private int evaporation_rate_id;
+    private double value;
+    private int product_id;
 }
